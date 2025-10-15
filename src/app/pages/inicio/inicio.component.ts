57fe33d39ddd91426d71b741842e1f51903c0075
CARLOSMEYER <20210696@uthh.edu.mx>
2025-10-15 11:23:42 -0600

    redes sociales listo

--- src/app/pages/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio.component.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { CsvService } from '../../../services/csv.service';
 
 @Component({
   selector: 'app-inicio',
-  imports: [],
+ standalone: true,
+  imports: [CommonModule, RouterLink],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
-export class InicioComponent {
+export class InicioComponent implements OnInit {
 
+    logoUrl: string = '';
+    nombreEmpresa: string ='';
+    eslogan: string= '';
+
+ constructor(private csvService: CsvService) {}
+
+  ngOnInit(): void {
+    this.cargarLogoDesdeCSV();
+  }
+     private cargarLogoDesdeCSV(): void {
+    this.csvService.obtenerDatos().subscribe({
+      next: (data) => {
+        if (data?.length > 0 && data[0].LOGO_URL ||
+           data?.length > 0 && data[0].NOMBRE_EMPRESA||
+           data?.length > 0 && data[0].ESLOGAN
+           ) {
+          this.logoUrl = data[0].LOGO_URL.trim();
+          this.nombreEmpresa = data[0].NOMBRE_EMPRESA.trim();
+          this.eslogan = data[0].ESLOGAN.trim();
+          console.log('cargado:');
+        } else {
+          console.warn('⚠️ No se encontró el campo en el CSV');
+        }
+      },
+      error: (err) => console.error('❌ Error al cargar CSV:', err)
+    });
+  
+  }
 }
+
